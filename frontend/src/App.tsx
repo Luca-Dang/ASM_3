7f260eb5a934dd62c3f8ad2accac408e0c95ed5e
Nguyen Dang <nguyendanglmv@gmail.com>
2024-02-10 02:07:04 -0500

    second commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {employeeFeedback} from "common/src/types";
+import axios from "axios";
+import './index.css';
+
+function App() {
+  return (
+      <div className={'max-h-screen flex flex-col'}>
+        <div className={'bg-amber-800'}>1</div>
+        <div>2</div>
+      </div>
+  )
+}
+
+export default App;
